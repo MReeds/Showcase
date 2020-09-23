5f4537b169df4266c5440c0782a8d057e7329249
MReeds <mattlreeds@gmail.com>
2020-09-23 11:33:54 -0500

    fixed bug

---
+++ src/components/Showcase.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./nav/navBar";
+
+const Showcase = () => {
+ 
+    return (
+        <>
+        <NavBar />
+        </>
+    )
+}
+
+export default Showcase
