42875c5bdd6ffe7d8ae048c17b72d1c626a74cbd
Your Name <you@example.com>
2020-09-29 15:27:18 -0500

    photo upload using cloudinary micro service is now working

--- src/components/user/newUserForm.js
+++ src/components/user/newUserForm.js
@@ -1,48 +1,54 @@
 import React, { useState } from "react";
-import * as firebase from "../../config/firebaseConfig.js";
-//add import for storage
+
+
+// //add import for storage
 const NewUserForm = (props) => {
-    const allInputs = { imgUrl: '' }
-    const [image, setImage] = useState(null)
-    const [imageAsUrl, setImageAsUrl] = useState(allInputs)
-    // console.log(imageAsFile)?
+    const [image, setImage] = useState("");
+    // const [imageAsUrl, setImageAsUrl] = useState('');
+    const [loading, setLoading] = useState(false)
 
 
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0])
-        }
-    }
+    // const handleChange = (e) => {
+    //     if (e.target.files[0]) {
+    //         setImage(e.target.files[0]);
+    //     }
+    // };
+    // console.log(image)
 
-   const handleUpload = () => {
-        const uploadTask = firebase.storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on(
-            "state_changed",
-            snapshot => {},
-            error => {
-                console.log(error);
-            },
-            () => {
-                firebase.storage.ref("images")
-                .child(image.name)
-                .getDownloadURL()
-                .then(url => {
+    const handleUpload = async e => {
+        let files = e.target.files
+        let data = new FormData()
+        data.append('file', files[0])
+        data.append('upload_preset', 'showcaseApp')
+        console.log(data)
+        setLoading(true)
+        let response = await fetch('https://api.cloudinary.com/v1_1/mreeds123/image/upload',
+        {
+            method: "POST",
+            body: data
+        })
+        let file = await response.json()
+        console.log(file)
+        setImage(file.secure_url)
+        setLoading(false)
+    };
 
-                })
-            }
-        )
 
-    }
 
 
     return (
         <div>
             <form>
-                <input
-                    type="file"
-                    onChange={handleChange}
-                />
-                <button onclick={handleUpload}>upload to firebase</button>
+                <input type="file" onChange={handleUpload} />
+                {
+                    loading ? (
+                        <h3>
+                            Loading...
+                        </h3>
+                    ) : (
+                            <img src={image} style={{ width: '300px' }} />
+                        )
+                }
             </form>
         </div>
     );
